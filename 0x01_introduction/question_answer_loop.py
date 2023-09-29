8bd0484dde17b18844275e88ae7bd3f434fb0b31
kipngenokevin <kipngenokevin254@gmail.com>
2023-09-29 17:37:38 +0300

    A fun game of questions and answers

--- 0x01_introduction/question_answer_loop.py
+++ 0x01_introduction/question_answer_loop.py
@@ -13,12 +13,19 @@ if len(questions) != len(answers):
     if len(questions) < len(answers):
         new_question_input = input(f"Kindly enter {len(answers) - len(questions)} more questions: ")
         new_questions = new_question_input.split()
+        if len(new_questions) != len(answers) - len(questions):
+            new_questions_input = input(f"Kindly enter {(len(answers) - len(questions)) - len(new_questions)} more questions: ")
+            new_questions.append(new_questions_input.split())
         #append the new input to the list
         questions.append(new_questions)
     elif len(answers) < len(questions):
-        new_answers = input(f"Kindly enter {len(questions) - len(answers)} more answers: ")
+        new_answers_input = input(f"Kindly enter {len(questions) - len(answers)} more answers: ")
+        new_answers = new_answers_input.split()
+        if len(new_answers) != len(questions) - len(answers):
+            new_answers_input = input(f"Kindly enter {(len(questions) - len(answers)) - len(new_answers)} more answers: ")
+            new_answers.append(new_answers_input.split())
         #append the new input to the list
-        answers.append(new_answers.split())
+        answers.append(new_answers)
 #use the for loop and zip()
 for q, a in zip(questions, answers):
     print(f"""What is your {q}?
